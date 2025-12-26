e2b44783d4d58e6ce7566978aa47d3c7ca897b91
KieuTher <myjuly1907@gmail.com>
2025-12-26 22:00:29 +0700

    KieuTho_Header_Footer

---
+++ index.php
@@ -0,0 +1,13 @@
+
+
+<?php
+require_once __DIR__ . '/views/layout/header.php'; ?>
+
+ <section class="banner">
+        <img src="/TNU/assets/images/banner.png" alt="Banner">
+    </section>
+
+   
+<?php
+require_once __DIR__ . '/views/layout/footer.php';
+?>
